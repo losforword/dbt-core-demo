35a02f4b1bec8cd6d19e49331702111ddceb2f00
Saurabh Tuladhar <saurabh.tuladhar@payback.net>
2024-10-27 09:48:22 +0100

    - Test commit

---
+++ models/stg_customers.sql
@@ -0,0 +1,5 @@
+select
+    id as customer_id,
+    first_name,
+    last_name
+from `sb-stul-20241016-155534`.ds_helloworld_de_customer.customers
---
+++ models/stg_orders.sql
@@ -0,0 +1,6 @@
+select
+    id as order_id,
+    user_id as customer_id,
+    order_date,
+    status
+from `sb-stul-20241016-155534`.ds_helloworld_de_customer.orders
